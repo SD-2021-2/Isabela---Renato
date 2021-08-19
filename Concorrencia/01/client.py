75e13c699bea21ea130988e6c19a01f0b25edb38
Renato S Dias <39096504+RSilvaDias@users.noreply.github.com>
2021-08-19 00:07:20 -0300

    Add files via upload

---
+++ Concorrencia/01/client.py
@@ -0,0 +1,21 @@
+import socket
+import json
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('LocalHost', 2020))
+print("Nome: ")
+nome = input()
+print("Cargo: ")
+cargo = input()
+print("Salario: ")
+salario = input()
+json1 = {
+    "nome": nome,
+    "cargo": cargo,
+    "salario": salario,
+}
+document = json.dumps(json1)
+s.sendall((document+"\n").encode())
+print("mensagem enviada")
+#resposta = s.recv(64)
+#print(resposta)
